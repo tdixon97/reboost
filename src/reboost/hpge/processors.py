05b42e6c645773017b30b4eefbf40a32cbf1f664
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2024-10-21 20:15:48 +0100

    pc fixes

--- src/reboost/hpge/processors.py
+++ src/reboost/hpge/processors.py
@@ -1,19 +1,20 @@
+from __future__ import annotations
+
 import awkward as ak
 import numpy as np
 
 
-
 def group_by_event(data):
-    counts = ak.run_lengths(data['evtid'])
+    counts = ak.run_lengths(data["evtid"])
 
     grouped = ak.unflatten(data, counts)
-    sum_energy = ak.sum(grouped.edep,axis=-1)
-        
-    t0 = ak.fill_none(ak.firsts(grouped.time,axis=-1),np.nan)
-    index = ak.fill_none(ak.firsts(grouped.evtid,axis=-1),np.nan)
- 
+    sum_energy = ak.sum(grouped.edep, axis=-1)
 
-    return ak.zip({"sum_energy":sum_energy,"t0":t0,"evtid":index})
+    t0 = ak.fill_none(ak.firsts(grouped.time, axis=-1), np.nan)
+    index = ak.fill_none(ak.firsts(grouped.evtid, axis=-1), np.nan)
 
-def smear_energy(energies,reso_func):
-    return np.random.normal(energies,reso_func(energies))
+    return ak.zip({"sum_energy": sum_energy, "t0": t0, "evtid": index})
+
+
+def smear_energy(energies, reso_func):
+    return np.random.normal(energies, reso_func(energies))
