97e912a6d304eb05be831dc86db68d5b71c134dc
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2025-06-17 16:21:14 +0200

    fix __init__.py

--- src/reboost/__init__.py
+++ src/reboost/__init__.py
@@ -2,20 +2,12 @@ from __future__ import annotations
 
 from lgdo import lh5
 
-from . import build_hit, core, iterator, math, shape
 from ._version import version as __version__
+from .build_hit import build_hit
 
 __all__ = [
     "__version__",
-    "build_glm",
     "build_hit",
-    "build_hit",
-    "build_tcm",
-    "core",
-    "iterator",
-    "math",
-    "optmap",
-    "shape",
 ]
 
 lh5.settings.DEFAULT_HDF5_SETTINGS = {"shuffle": True, "compression": "lzf"}
