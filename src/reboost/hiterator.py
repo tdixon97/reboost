228dcd75a3c933eff253e9a873f22ca79aeb8858
Toby Dixon <tdixon@berkeley.edu>
2025-09-17 22:39:02 +0200

    make the detector get passed directly to the hiterator and a bunch of fixes

--- src/reboost/hiterator.py
+++ src/reboost/hiterator.py
@@ -31,7 +31,8 @@ class Hiterator:
         input_files: str | list[str],
         glm_files: str | list[str] | None,
         reshaped_files: bool = True,
-        detectors_mapping: dict[str, list[str]],
+        input_detector: str = "",
+        output_detectors: str | list = "",
         input_hdf5_group: str = "stp",
         start_hit: int = 0,
         max_n_hits: int | None = None,
@@ -41,8 +42,9 @@ class Hiterator:
         self.files = utils.get_file_dict(stp_files=input_files, glm_files=glm_files)
         self.reshaped_files = reshaped_files
 
-        self.detectors_mapping = detectors_mapping
         self.input_hdf5_group = input_hdf5_group
+        self.input_detector = input_detector
+        self.output_detectors = output_detectors
 
         self.start_hit = start_hit
         self.max_n_hits = max_n_hits
@@ -64,33 +66,31 @@ class Hiterator:
                 msg = f"starting processing of {stp_file}"
                 log.info(msg)
 
-            # now loop over detectors
-            for in_detector, out_detectors in self.detectors_mapping.items():
-                msg = f"processing {in_detector} (to {out_detectors})"
-                log.debug(msg)
+            msg = f"processing {self.input_detector} (to {self.output_detectors})"
+            log.debug(msg)
 
-                iterator = GLMIterator(
-                    glm_file,
-                    stp_file,
-                    lh5_group=in_detector,
-                    start_row=self.start_hit,
-                    stp_field=self.input_hdf5_group,
-                    n_rows=self.max_n_hits,
-                    buffer=self.buffer_size_rows,
-                    time_dict=self.time_dict,
-                    reshaped_files=self.reshaped_files,
-                )
+            iterator = GLMIterator(
+                glm_file,
+                stp_file,
+                lh5_group=self.input_detector,
+                start_row=self.start_hit,
+                stp_field=self.input_hdf5_group,
+                n_rows=self.max_n_hits,
+                buffer=self.buffer_size_rows,
+                time_dict=self.time_dict,
+                reshaped_files=self.reshaped_files,
+            )
 
-                for data, _, _ in iterator:
-                    if data is None:
-                        continue
+            for data, _, _ in iterator:
+                if data is None:
+                    continue
 
-                    self.time_dict.update_field("conv", time.time())
+                self.time_dict.update_field("conv", time.time())
 
-                    for out_detector in out_detectors:
-                        yield HitContext(
-                            data=data,
-                            in_detector=in_detector,
-                            out_detector=out_detector,
-                            time_dict=self.time_dict,
-                        )
+                for out_detector in self.output_detectors:
+                    yield HitContext(
+                        data=data,
+                        in_detector=self.input_detector,
+                        out_detector=out_detector,
+                        time_dict=self.time_dict,
+                    )
--- tests/hit/test_hiterator.py
+++ tests/hit/test_hiterator.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import copy
 from pathlib import Path
 
 import awkward as ak
@@ -8,13 +9,47 @@ import lgdo
 import numpy as np
 import pyg4ometry
 import pygeomtools
+import pytest
 from dbetto import AttrsDict
-from lgdo import lh5
+from lgdo import Array, Table, VectorOfVectors, lh5
 
-import reboost
 from reboost import Hiterator
+from reboost.hpge import surface
+from reboost.math import functions, stats
 
 configs = Path(__file__).parent / "configs"
+
+
+@pytest.fixture(scope="module")
+def test_gen_lh5(tmptestdir):
+    # write a basic lh5 file
+
+    stp_path = str(tmptestdir / "basic.lh5")
+
+    data = {}
+    data["evtid"] = Array([0, 1])
+    data["edep"] = VectorOfVectors([[100, 200], [10, 20, 300]])  # keV
+    data["time"] = VectorOfVectors([[0, 1.5], [0.1, 2.1, 3.7]])  # ns
+
+    data["xloc"] = VectorOfVectors([[0.01, 0.02], [0.001, 0.003, 0.005]])  # m
+    data["yloc"] = VectorOfVectors([[0.01, 0.02], [0.001, 0.003, 0.005]])  # m
+    data["zloc"] = VectorOfVectors([[0.04, 0.02], [0.001, 0.023, 0.005]])  # m
+    data["dist_to_surf"] = VectorOfVectors([[0.04, 0.02], [0.011, 0.003, 0.051]])  # m
+
+    vertices = [0, 1]
+    tab = Table(data)
+    tab2 = copy.deepcopy(tab)
+
+    lh5.write(tab, "stp/det1", stp_path, wo_mode="of")
+    lh5.write(tab2, "stp/det2", stp_path, wo_mode="append")
+    lh5.write(
+        Table({"evtid": Array(vertices)}),
+        "vtx",
+        stp_path,
+        wo_mode="append",
+    )
+
+    return stp_path
 
 
 def test_full_chain(test_gen_lh5, tmptestdir):
@@ -22,68 +57,64 @@ def test_full_chain(test_gen_lh5, tmptestdir):
 
     pars = AttrsDict(
         {
-            "det1": {
-                "fccd_in_mm": 0.71,
-                "dlf": 0.7,
-                "reso_in_sigma": 1.1,
-            },
-            "det2": {
-                "fccd_in_mm": 2.2,
-                "dlf": 0.4,
-                "reso_in_sigma": 2.32,
-            },
+            "det1": {"fccd_in_mm": 0.71, "dlf": 0.7, "reso_in_sigma": 1.1, "name": "BEGe"},
+            "det2": {"fccd_in_mm": 2.2, "dlf": 0.4, "reso_in_sigma": 2.32, "name": "Coax"},
         }
     )
 
     geometry = pyg4ometry.gdml.Reader(str(configs / "geom.gdml")).getRegistry()
     registry = pyg4ometry.geant4.Registry()
 
-    metadata = AttrsDict({k: pygeomtools.get_sensvol_metadata(geometry, k) for k in detectors})
-
-    hpges = {k: legendhpges.make_hpge(metadata[k], registry=registry) for k in detectors}
-
-    it = Hiterator(
-        input_files=test_gen_lh5,
-        glm_files=None,
-        detectors_mapping={"output": detectors},
-        input_hdf5_group="stp",
-        buffer_size_rows=5_000_000,
-        overwrite=True,
+    metadata = AttrsDict(
+        {k: pygeomtools.get_sensvol_metadata(geometry, pars[k].name) for k in detectors}
     )
 
-    for chunk in it:
-        _t0 = ak.fill_none(ak.firsts(chunk.data.time, axis=-1), np.nan)
-
-        _distance = reboost.hpge.surface.distance_to_surface(
-            chunk.data.xloc,
-            chunk.data.yloc,
-            chunk.data.zloc,
-            hpges[chunk.in_detector],
-            geometry.physicalVolumeDict[chunk.in_detector].position.eval(),
-            distances_precompute=chunk.data.dist_to_surf,
-            precompute_cutoff=0.002,
-            surface_type="nplus",
+    for det in detectors:
+        it = Hiterator(
+            input_files=test_gen_lh5,
+            glm_files=None,
+            input_detector=det,
+            output_detectors=det,
+            input_hdf5_group="stp",
+            buffer_size_rows=5_000_000,
+            overwrite=True,
         )
 
-        _activeness = reboost.math.functions.piecewise_linear_activeness(
-            _distance, fccd=pars[chunk.in_detector].fccd_in_mm, dlf=pars[chunk.in_detector].dlf
-        )
+        # get some objects
+        hpge = legendhpges.make_hpge(metadata[det], registry=registry)
+        name = pars[det].name
 
-        _active_e = ak.sum(chunk.data.edep * _activeness, axis=-1)
+        for chunk in it:
+            _t0 = ak.fill_none(ak.firsts(chunk.data.time, axis=-1), np.nan)
 
-        _smeared_e = reboost.math.stats.gaussian_sample(
-            chunk.data.active_energy, pars[chunk.in_detector].reso_in_sigma
-        )
+            _distance = surface.distance_to_surface(
+                chunk.data.xloc,
+                chunk.data.yloc,
+                chunk.data.zloc,
+                hpge,
+                geometry.physicalVolumeDict[name].position.eval(),
+                distances_precompute=chunk.data.dist_to_surf,
+                precompute_cutoff=0.002,
+                surface_type="nplus",
+            )
 
-        table = lgdo.Table()
-        table.add_field("t0", lgdo.Array(_t0))
-        table.add_field("distance_to_nplus", lgdo.VectorOfVectors(_distance))
-        table.add_field("activeness", lgdo.VectorOfVectors(_activeness))
-        table.add_field("active_energy", lgdo.Array(_active_e))
-        table.add_field("smeared_energy", lgdo.Array(_smeared_e))
+            _activeness = functions.piecewise_linear_activeness(
+                _distance, fccd=pars[chunk.in_detector].fccd_in_mm, dlf=pars[chunk.in_detector].dlf
+            ).view_as("ak")
 
-        lh5.write(
-            table,
-            f"hit/{chunk.out_detector}",
-            tmptestdir / "beta-small-hiterator.lh5",
-        )
+            _active_e = ak.sum(chunk.data.edep.view_as("ak") * _activeness, axis=-1)
+
+            _smeared_e = stats.gaussian_sample(_active_e, pars[chunk.in_detector].reso_in_sigma)
+
+            table = lgdo.Table()
+            table.add_field("t0", lgdo.Array(_t0))
+            table.add_field("distance_to_nplus", lgdo.VectorOfVectors(_distance))
+            table.add_field("activeness", lgdo.VectorOfVectors(_activeness))
+            table.add_field("active_energy", lgdo.Array(_active_e))
+            table.add_field("smeared_energy", lgdo.Array(_smeared_e))
+
+            lh5.write(
+                table,
+                f"hit/{chunk.out_detector}",
+                tmptestdir / "beta-small-hiterator.lh5",
+            )
